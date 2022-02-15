24fcbf2ed65757899c09f39402a125c576b320f5
William Barbosa <will.silver@hotmail.com>
2022-02-15 13:45:52 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Excercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
